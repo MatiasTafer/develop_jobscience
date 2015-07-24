bc623da65ff6cc620bf9e12e24beedb7cb4ca8d1
martinbazzano <martinb@oktana.io>
2015-07-24 14:29:01 -0300

    Added constants and now log out is also tested

--- lib/New_Automation/test_login.rb
+++ lib/New_Automation/test_login.rb
@@ -4,118 +4,143 @@ require 'selenium-webdriver'
 require 'test-unit'
 
 class TestLogin < TestBasic
+
+    LOG_IN_LINK_ID = "loginLink"
+    LOG_IN_MODAL_XPATH = ".//*[@class='modal-dialog modal-md']"
+    EMAIL_FIELD_XPATH = ".//*[@name='email']"
+    PASSWORD_FIELD_XPATH = ".//*[@name='password']"
+    LOG_IN_BUTTON_XPATH = ".//*[@class='btn btn-primary btn-lg btn-block']"
+    MY_ACCOUNT_LINK_XPATH = ".//*[@class='dropdown dropdown-hover with-arrow']//*[@href='/account/info']"
+    MISMATCH_MESSAGE_XPATH = ".//*[@class='form-message-block text-center text-danger']"
+    PASSWORD_ERROR_MESSAGE_XPATH = ".//*[@ng-show='isPasswordInvalid()']"
+    EMAIL_ERROR_MESSAGE_XPATH = ".//*[@ng-show='isEmailInvalid()']"
+    LOG_OUT_LINK_ID = "account-menu-logout"
+
     
 	# Check that login link is there
 	def login_link_existence
     	assert $wait.until {
-        	$browser.find_element(:id, "loginLink").displayed?
+        	$browser.find_element(:id, LOG_IN_LINK_ID).displayed?
     	}
 	end
  
 	# Checking the modal appears 
 	def test_login_modal
-    	$browser.find_element(:id, "loginLink").click  
+    	$browser.find_element(:id, LOG_IN_LINK_ID).click  
     	assert $wait.until {
-        	$browser.find_element(:xpath => ".//*[@class='modal-dialog modal-md']").displayed?
+        	$browser.find_element(:xpath => LOG_IN_MODAL_XPATH).displayed?
     	}
 	end
 
-    # Logging in correctly
-    def test_login_ok
-        $browser.find_element(:id, "loginLink").click
+    # Logging in and logging out correctly
+    def test_login_logout_ok
+        $browser.find_element(:id, LOG_IN_LINK_ID).click
         $wait.until{
-            $browser.find_element(:xpath => ".//*[@name='email']").displayed?
+            $browser.find_element(:xpath => EMAIL_FIELD_XPATH).displayed?
         }
-        $browser.find_element(:xpath => ".//*[@name='email']").send_keys 'complete@test.com'      
-        $browser.find_element(:xpath => ".//*[@name='password']").send_keys 'password'
-        $browser.find_element(:xpath => ".//*[@class='btn btn-primary btn-lg btn-block']").submit
+        $browser.find_element(:xpath => EMAIL_FIELD_XPATH).send_keys 'complete@test.com'      
+        $browser.find_element(:xpath => PASSWORD_FIELD_XPATH).send_keys 'password'
+        $browser.find_element(:xpath => LOG_IN_BUTTON_XPATH).submit
         assert $wait.until {
-            $browser.find_element(:xpath => ".//*[@class='dropdown dropdown-hover with-arrow']//*[@href='/account/info']").displayed?
+            $browser.find_element(:xpath => MY_ACCOUNT_LINK_XPATH).displayed?
+        }
+        $browser.action.move_to($browser.find_element(:xpath => MY_ACCOUNT_LINK_XPATH)).perform
+        $wait.until{
+            $browser.find_element(:id, LOG_OUT_LINK_ID).displayed?
+        }
+        $browser.find_element(:id, LOG_OUT_LINK_ID).click
+        assert $wait.until{
+            $browser.find_element(:id, LOG_IN_LINK_ID).displayed?
         }
     end
 
     # Trying to log in with an unregistered user
     def test_login_unregistered_user
-        $browser.find_element(:id, "loginLink").click
+        $browser.find_element(:id, LOG_IN_LINK_ID).click
         $wait.until{
-            $browser.find_element(:xpath => ".//*[@name='email']").displayed?
+            $browser.find_element(:xpath => EMAIL_FIELD_XPATH).displayed?
         }
-        $browser.find_element(:xpath => ".//*[@name='email']").send_keys 'unregistered_user@test.com'      
-        $browser.find_element(:xpath => ".//*[@name='password']").send_keys 'password'
-        $browser.find_element(:xpath => ".//*[@class='btn btn-primary btn-lg btn-block']").submit        
+        $browser.find_element(:xpath => EMAIL_FIELD_XPATH).send_keys 'unregistered_user@test.com'      
+        $browser.find_element(:xpath => PASSWORD_FIELD_XPATH).send_keys 'password'
+        $browser.find_element(:xpath => LOG_IN_BUTTON_XPATH).submit        
         $wait.until {
-            $browser.find_element(:xpath => ".//*[@class='form-message-block text-center text-danger']").displayed?
+            $browser.find_element(:xpath => MISMATCH_MESSAGE_XPATH).displayed?
         }
-        assert $browser.find_element(:xpath => ".//*[@class='form-message-block text-center text-danger']").text == 'Invalid username or password'
+        assert $browser.find_element(:xpath => MISMATCH_MESSAGE_XPATH).text == 'Invalid username or password'
     end
 
     # Trying to log in with a registered user but using a wrong password
     def test_login_wrong_password
-        $browser.find_element(:id, "loginLink").click
+        $browser.find_element(:id, LOG_IN_LINK_ID).click
         $wait.until{
-            $browser.find_element(:xpath => ".//*[@name='email']").displayed?
+            $browser.find_element(:xpath => EMAIL_FIELD_XPATH).displayed?
         }
-        $browser.find_element(:xpath => ".//*[@name='email']").send_keys 'complete@test.com'      
-        $browser.find_element(:xpath => ".//*[@name='password']").send_keys 'wrongpassword'
-        $browser.find_element(:xpath => ".//*[@class='btn btn-primary btn-lg btn-block']").submit        
+        $browser.find_element(:xpath => EMAIL_FIELD_XPATH).send_keys 'complete@test.com'      
+        $browser.find_element(:xpath => PASSWORD_FIELD_XPATH).send_keys 'wrongpassword'
+        $browser.find_element(:xpath => LOG_IN_BUTTON_XPATH).submit        
         $wait.until {
-            $browser.find_element(:xpath => ".//*[@class='form-message-block text-center text-danger']").displayed?
+            $browser.find_element(:xpath => MISMATCH_MESSAGE_XPATH).displayed?
         }
-        assert $browser.find_element(:xpath => ".//*[@class='form-message-block text-center text-danger']").text == 'Invalid username or password'
+        assert $browser.find_element(:xpath => MISMATCH_MESSAGE_XPATH).text == 'Invalid username or password'
     end
 
+    # Trying to log in with a registered user but leaving the password field blank
     def test_login_blank_password
-        $browser.find_element(:id, "loginLink").click
+        $browser.find_element(:id, LOG_IN_LINK_ID).click
         $wait.until{
-            $browser.find_element(:xpath => ".//*[@name='email']").displayed?
+            $browser.find_element(:xpath => EMAIL_FIELD_XPATH).displayed?
         }
-        $browser.find_element(:xpath => ".//*[@name='email']").send_keys 'complete@test.com' 
-        $browser.find_element(:xpath => ".//*[@class='btn btn-primary btn-lg btn-block']").submit        
+        $browser.find_element(:xpath => EMAIL_FIELD_XPATH).send_keys 'complete@test.com' 
+        $browser.find_element(:xpath => LOG_IN_BUTTON_XPATH).submit        
         $wait.until {
-            $browser.find_element(:xpath => ".//*[@ng-show='isPasswordInvalid()']").displayed?
+            $browser.find_element(:xpath => PASSWORD_ERROR_MESSAGE_XPATH).displayed?
         }
-        assert $browser.find_element(:xpath => ".//*[@ng-show='isPasswordInvalid()']").text == 'Please enter your password.'
+        assert $browser.find_element(:xpath => PASSWORD_ERROR_MESSAGE_XPATH).text == 'Please enter your password.'
     end
 
+    # Trying to log in leaving the username field blank
     def test_login_blank_username
-        $browser.find_element(:id, "loginLink").click
+        $browser.find_element(:id, LOG_IN_LINK_ID).click
         $wait.until{
-            $browser.find_element(:xpath => ".//*[@name='email']").displayed?
+            $browser.find_element(:xpath => EMAIL_FIELD_XPATH).displayed?
         }
-        $browser.find_element(:xpath => ".//*[@name='password']").send_keys 'password' 
-        $browser.find_element(:xpath => ".//*[@class='btn btn-primary btn-lg btn-block']").submit        
+        $browser.find_element(:xpath => PASSWORD_FIELD_XPATH).send_keys 'password' 
+        $browser.find_element(:xpath => LOG_IN_BUTTON_XPATH).submit        
         $wait.until {
-            $browser.find_element(:xpath => ".//*[@ng-show='isEmailInvalid()']").displayed?
+            $browser.find_element(:xpath => EMAIL_ERROR_MESSAGE_XPATH).displayed?
         }
-        assert $browser.find_element(:xpath => ".//*[@ng-show='isEmailInvalid()']").text == 'Please enter a valid email address.'
+        assert $browser.find_element(:xpath => EMAIL_ERROR_MESSAGE_XPATH).text == 'Please enter a valid email address.'
     end
 
+    # Trying to log in using an email address with the @ sign missing
     def test_login_at_sign_missing
-        $browser.find_element(:id, "loginLink").click
+        $browser.find_element(:id, LOG_IN_LINK_ID).click
         $wait.until{
-            $browser.find_element(:xpath => ".//*[@name='email']").displayed?
+            $browser.find_element(:xpath => EMAIL_FIELD_XPATH).displayed?
         }
-        $browser.find_element(:xpath => ".//*[@name='email']").send_keys 'completetest.com' 
-        $browser.find_element(:xpath => ".//*[@name='password']").send_keys 'password' 
-        $browser.find_element(:xpath => ".//*[@class='btn btn-primary btn-lg btn-block']").submit        
+        $browser.find_element(:xpath => EMAIL_FIELD_XPATH).send_keys 'completetest.com' 
+        $browser.find_element(:xpath => PASSWORD_FIELD_XPATH).send_keys 'password' 
+        $browser.find_element(:xpath => LOG_IN_BUTTON_XPATH).submit        
         $wait.until {
-            $browser.find_element(:xpath => ".//*[@ng-show='isEmailInvalid()']").displayed?
+            $browser.find_element(:xpath => EMAIL_ERROR_MESSAGE_XPATH).displayed?
         }
-        assert $browser.find_element(:xpath => ".//*[@ng-show='isEmailInvalid()']").text == 'Please enter a valid email address.'
+        assert $browser.find_element(:xpath => EMAIL_ERROR_MESSAGE_XPATH).text == 'Please enter a valid email address.'
     end
 
+    # Trying to log in using an email address with the dot missing
     def test_login_dot_missing
-        $browser.find_element(:id, "loginLink").click
+        $browser.find_element(:id, LOG_IN_LINK_ID).click
         $wait.until{
-            $browser.find_element(:xpath => ".//*[@name='email']").displayed?
+            $browser.find_element(:xpath => EMAIL_FIELD_XPATH).displayed?
         }
-        $browser.find_element(:xpath => ".//*[@name='email']").send_keys 'complete@testcom' 
-        $browser.find_element(:xpath => ".//*[@name='password']").send_keys 'password' 
-        $browser.find_element(:xpath => ".//*[@class='btn btn-primary btn-lg btn-block']").submit        
+        $browser.find_element(:xpath => EMAIL_FIELD_XPATH).send_keys 'complete@testcom' 
+        $browser.find_element(:xpath => PASSWORD_FIELD_XPATH).send_keys 'password' 
+        $browser.find_element(:xpath => LOG_IN_BUTTON_XPATH).submit        
         $wait.until {
-            $browser.find_element(:xpath => ".//*[@ng-show='isEmailInvalid()']").displayed?
+            $browser.find_element(:xpath => EMAIL_ERROR_MESSAGE_XPATH).displayed?
         }
-        assert $browser.find_element(:xpath => ".//*[@ng-show='isEmailInvalid()']").text == 'Please enter a valid email address.'
+        assert $browser.find_element(:xpath => EMAIL_ERROR_MESSAGE_XPATH).text == 'Please enter a valid email address.'
     end
+
 
 end
