fb600cf48cefc628a1ffebcc8aba63504a99dede
Martin <martinds@oktana.io>
2015-08-06 17:38:26 -0300

    Test login only

--- lib/New_Automation.rb
+++ lib/New_Automation.rb
@@ -5,7 +5,7 @@ $wait
 
 
 require './New_Automation/test_login'
-require './New_Automation/test_registry_creation'
+#require './New_Automation/test_registry_creation'
 #require './New_Automation/test_sign_up'
-require './New_Automation/test_change_password'
+#require './New_Automation/test_change_password'
 #require './New_Automation/test_your_information'
