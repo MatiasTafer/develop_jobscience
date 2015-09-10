38ba7084a2f3841d3885c7140d6be6bccc03a18a
Martin <martinds@oktana.io>
2015-09-10 12:57:47 -0300

    test

--- lib/New_Automation.rb
+++ lib/New_Automation.rb
@@ -9,8 +9,8 @@ $wait
 #require './New_Automation/test_change_password'
 #require './New_Automation/test_your_information'
 require './New_Automation/test_shipping_info'
-#require './New_Automation/test_registry_settings'
-#require './New_Automation/test_footer_links'
+require './New_Automation/test_registry_settings'
+require './New_Automation/test_footer_links'
 #require './New_Automation/test_cash_gift_settings'
 #require './New_Automation/test_new_signup'
 #DEPRECATED BECAUSE A NEW SIGNUP FLOW WAS MADE
