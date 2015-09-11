24a6824f6aa3d09a28f5f11f05802a6f6b91f91a
Martin <martinds@oktana.io>
2015-09-11 10:48:09 -0300

    On: Login

--- lib/New_Automation.rb
+++ lib/New_Automation.rb
@@ -4,11 +4,11 @@ $browser
 $wait
 
 
-#require './New_Automation/test_login'
+require './New_Automation/test_login'
 #require './New_Automation/test_registry_creation'
 #require './New_Automation/test_change_password'
 #require './New_Automation/test_your_information'
-require './New_Automation/test_shipping_info'
+#require './New_Automation/test_shipping_info'
 #require './New_Automation/test_registry_settings'
 #require './New_Automation/test_footer_links'
 #require './New_Automation/test_cash_gift_settings'
