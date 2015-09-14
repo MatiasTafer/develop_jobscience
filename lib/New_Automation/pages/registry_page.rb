e2d1e8a9e7ce3e26eb1390342c430b2285a02d4d
martinbazzano <martinb@oktana.io>
2015-09-14 15:44:12 -0300

    Cart Test Suite was reviewed and refactored

--- lib/New_Automation/pages/registry_page.rb
+++ lib/New_Automation/pages/registry_page.rb
@@ -2,9 +2,10 @@ class RegistryPage
   
   PRODUCT_TO_USE_XPATH = ".//*[@class='add-to-cart-action button small primary add-to-cart add-to-cart-available']"
   ERROR_MESSAGE_ID = "humaneMessage"
-  ERROR_MESSAGE_TEXT_ID = "Cannot add items that belong to multiple registries.  Please checkout existing items first."
+  ERROR_MESSAGE_TEXT_ID = "Cannot add items that belong to multiple registries. Please checkout existing items first."
+  ERROR_MESSAGE_TEXT2_ID = "Quantity is over the maximum requested on registry"
   OTHER_REGISTRY_PRODUCT_ID = "quick_addCart_ascot_chesirebasket"
-  PRODUCT_QUANTITY_PROOF_ID = "acaciaware_s4calabashbwl"
   PRODUCT_QUANTITY_BUTTON_PROOF_ID = "quick_addCart_acaciaware_s4calabashbwl"
+  PRODUCT_QUANTITY_FIELD_PROOF_XPATH = ".//*[@id='acaciaware_s4calabashbwl']//*[@name='quantity']"
   OTHER_REGISTRY_CART_XPATH = ".//*[@class='cart']"
 end
