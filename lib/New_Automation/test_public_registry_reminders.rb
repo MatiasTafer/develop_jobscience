d2eba248492d34c7a2e894957bb825230e3a5725
admin <admin@RodrigoCastro.local>
2015-09-22 13:09:18 -0300

    Add coments

--- lib/New_Automation/test_public_registry_reminders.rb
+++ lib/New_Automation/test_public_registry_reminders.rb
@@ -13,36 +13,25 @@ require_relative './pages/home_page.rb'
 
 class TestPublicRegistryReminders < TestBasic
   
-  
-  
-  
-  
   #TC1128 MAKE IT VISIBLE TO GUESTS WITH NOT SAVED SHIPPING ADDRESS
   def test_visible_to_guests_not_address
+    #Login
     Common.login(Common::USER2_EMAIL, Common::USER23_password)
     $wait.until {
       $browser.find_element(:xpath => HomePage::MY_ACCOUNT_LINK_XPATH).displayed?
     }
+    #Go to registry page
     $browser.get HomePage::HOME_URL
+    #Waiting for Make Registry Visible modal appears
     $wait.until{
       $browser.find_element(:xpath, MakeRegistryVisible::MAKE_IT_VISIBLE_BUTTON_XPATH).displayed?
     }
+    #Click on Make it visible button
     $browser.find_element(:xpath, MakeRegistryVisible::MAKE_IT_VISIBLE_BUTTON_XPATH).click
+    #Verify if Shipping Information Modal appears
     assert $wait.until{
       $browser.find_element(:id, ShippingInfoModal::FIRST_NAME_FIELD_ID).displayed?
     }
   end
-  
-  
-  #TC1128 MAKE IT VISIBLE TO GUESTS WITH SAVED SHIPPING ADDRESS
-  
-  
-  
-  
-  
-  
-  
-  
-  
   
 end
