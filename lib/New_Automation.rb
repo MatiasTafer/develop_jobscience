9087dcd7881c7d0aafe9f5bd37e600394a4098e7
Martin <martinds@oktana.io>
2015-10-01 14:20:45 -0300

    Enable footer links and login

--- lib/New_Automation.rb
+++ lib/New_Automation.rb
@@ -4,7 +4,7 @@ $browser
 $wait
 
 
-#require './New_Automation/test_login'
+require './New_Automation/test_login'
 #require './New_Automation/test_registry_creation'
 #require './New_Automation/test_change_password'
 #require './New_Automation/test_your_information'
