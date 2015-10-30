f1473500e4c815b5e5997b3cab149b131e6619b6
Martin <martinds@oktana.io>
2015-10-30 16:11:24 -0300

    Test change password

--- lib/New_Automation.rb
+++ lib/New_Automation.rb
@@ -3,7 +3,8 @@ $browserName = ARGV[0]
 $browser
 $wait
 
-require './New_Automation/test_login'
+#require './New_Automation/test_login'
+require './New_Automation/test_change_password'
 
 #require './New_Automation/test_cart'
 #require './New_Automation/test_product_modal'
