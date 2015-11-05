a9224ef5a0e144555e7503aec0e185199dbe93e2
Martin <martinds@oktana.io>
2015-11-05 16:38:11 -0300

    Added old signup

--- lib/New_Automation.rb
+++ lib/New_Automation.rb
@@ -11,16 +11,18 @@ $wait
 #require './New_Automation/test_new_signup'
 #require './New_Automation/test_footer_links'
 #require './New_Automation/test_forgot_password'
+#require './New_Automation/test_sign_up'
+
+
 
 #require './New_Automation/test_cart'
 #require './New_Automation/test_product_modal'
 #require './New_Automation/test_standalone_pdp'
 #require './New_Automation/test_registry_creation'
-require './New_Automation/test_registry_settings'
+#require './New_Automation/test_registry_settings'
 #require './New_Automation/test_cash_gift_settings'
 
 #DEPRECATED BECAUSE A NEW SIGNUP FLOW WAS MADE
-#require './New_Automation/test_sign_up'
 #require './New_Automation/test_recalculation_group-gifts'
 #require './New_Automation/test_public_registry_reminders'
 #require './New_Automation/test_manage_collections_registry_page'
