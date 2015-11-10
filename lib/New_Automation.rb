404a598a24c9f172bb2945f2d4e1902b449dd287
Martin <martinds@oktana.io>
2015-11-10 11:16:35 -0300

    test

--- lib/New_Automation.rb
+++ lib/New_Automation.rb
@@ -13,8 +13,8 @@ require './New_Automation/test_forgot_password'
 require './New_Automation/test_registry_settings'
 require './New_Automation/test_sign_up'
 require './New_Automation/test_sign_up'
-require './New_Automation/test_cash_gift_settings'
 =end
+require './New_Automation/test_cash_gift_settings'
 require './New_Automation/test_promotion_pages.rb'
 
 #require './New_Automation/test_cart'
