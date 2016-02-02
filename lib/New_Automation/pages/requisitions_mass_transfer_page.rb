6360494e725e324b4c8c6708abc39c230b5cfd27
admin <admin@Matias-Quintana.local>
2016-02-02 17:49:21 -0300

    Requisitions

---
+++ lib/New_Automation/pages/requisitions_mass_transfer_page.rb
@@ -0,0 +1,5 @@
+class RequisitionMassTransfer
+  REQUISITIONS_MASS_TRANSFER_TRANSFER_TO_RECRUITER_XPATH = ".//*[@class='detailList']/tbody/tr/td[1]/span/input"
+  REQUISITIONS_MASS_TRANSFER_BTN_TRANSFER_XPATH = "(.//*[@type='button'])[2]"
+  REQUISITIONS_MASS_TRANSFER_BTN_BACK_TO_LIST_XPATH = ".//*[@class='btn'][2]"
+end
--- lib/New_Automation/pages/requisitions_page.rb
+++ lib/New_Automation/pages/requisitions_page.rb
@@ -1,3 +1,7 @@
 class RequisitionsPage
   REQUISITIONS_PAGE_NEW_XPATH = ".//*[@name='new']"
+  REQUISITIONS_PAGE_TRANSFER_JOBS_XPATH = "(.//*[@class='outer']/tbody//*[@class='btn'])[3]"
+  
+  REQUISITIONS_PAGE_FIRST_ENTRY_LIST_TITLE_XPATH = "(.//*[@class='x-grid3-cell-inner x-grid3-col-NAME'])[1]/a"
+  REQUISITIONS_PAGE_FIRST_ENTRY_LIST_PRIMARY_RECRUITER_TEXT_XPATH = "(.//table)[4]/tbody/tr/td[9]//span"
 end
