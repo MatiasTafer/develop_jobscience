c15669a701e1a6ff78318920b89f3970408cadeb
rodrigoc@oktana.io <cmj3139cmj3139>
2016-02-02 12:12:24 -0200

    Finish Tab links

--- lib/New_Automation/pages/home_page.rb
+++ lib/New_Automation/pages/home_page.rb
@@ -2,6 +2,12 @@ class HomePage
   
   #TAB LINKS
   HOME_TAB_LINK_ID = "home_Tab"
-  
+  SHORT_LIST_TAB_LINK_XPATH = ".//*[@id='tabBar']//*[@class='wt-s_UserList']"
+  POWER_SEARCH_TAB_LINK_XPATH = ".//*[@id='tabBar']//*[@class='wt-PowerSearch']"
+  APPLICATION_TAB_LINK_XPATH = ".//*[@id='tabBar']//*[@class='wt-Application']"
+  REQUISITION_TAB_LINK_XPATH = ".//*[@id='tabBar']//*[@class='wt-Job']"
+  PRESENTATION_TAB_LINK_XPATH = ".//*[@id='tabBar']//*[@class='wt-Presentation']"
+  CONTACT_TAB_LINK_ID = "Contact_Tab"
+  AMS_SETUP_TAB_LINK_XPATH = ".//*[@id='tabBar']//*[@class='wt-AMS-Setup']"
   
 end
