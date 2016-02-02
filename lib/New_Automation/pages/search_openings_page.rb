b401da998fafc5fbf236f8bee27a02da93a3519b
rodrigoc@oktana.io <cmj3139cmj3139>
2016-02-02 16:59:22 -0200

    SearchOpenings and sites page completed

---
+++ lib/New_Automation/pages/search_openings_page.rb
@@ -0,0 +1,15 @@
+class SearchOpenings
+  
+  JOBS_PROJECTS_DROPDOWN_XPATH = "(.//*[@class='atsSearchTdRight'])[1]//*[@class='atsSelect']"
+  DEPARTAMENT_DROPDOWN_XPATH = "(.//*[@class='atsSearchTdRight'])[2]//*[@class='atsSelect']"
+  LOCATION_DROPDOWN_XPATH = "(.//*[@class='atsSearchTdRight'])[3]//*[@class='atsSelect']"
+  SEARCH_BY_KEYWORD_FIELD_1_XPATH = "(.//*[@class='atsSearchTdRight'])[4]/input[1]"
+  SEARCH_BY_KEYWORD_FIELD_2_XPATH ="(.//*[@class='atsSearchTdRight'])[4]/input[2]"
+  SEARCH_BY_KEYWORD_FIELD_3_XPATH = "(.//*[@class='atsSearchTdRight'])[4]/input[3]"
+  SEARCH_BUTTON_XPATH = "(.//*[@class='atsSearchTdRight'])[5]/input[1]"
+  CLEAR_FILTERS_BUTTON_XPATH = "(.//*[@class='atsSearchTdRight'])[5]/input[1]"
+  
+  
+  
+end
+  
