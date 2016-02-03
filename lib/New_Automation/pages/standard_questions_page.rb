1f4c09a8e37848b1b3711062a32e6af578017a60
rodrigoc@oktana.io <cmj3139cmj3139>
2016-02-03 20:14:36 -0200

    standar questions maped

---
+++ lib/New_Automation/pages/standard_questions_page.rb
@@ -0,0 +1,9 @@
+class StandardQuestions
+  
+  NEW_BUTTON_XPATH = ".//*[@class='pbButton']//*[@class='btn']"
+  FIRST_QUESTION_NAME_ITEM = ".//*[@class='list']//*[@class='dataRow even first']/th/a"
+  
+  
+  
+  
+end
