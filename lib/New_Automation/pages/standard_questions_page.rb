8caefff8380572348031d1b02ca7f0ab6afed1fd
rodrigoc@oktana.io <cmj3139cmj3139>
2016-02-04 16:01:25 -0200

    Test 928 completed

--- lib/New_Automation/pages/standard_questions_page.rb
+++ lib/New_Automation/pages/standard_questions_page.rb
@@ -1,7 +1,7 @@
 class StandardQuestions
   
   NEW_BUTTON_XPATH = ".//*[@class='pbButton']//*[@class='btn']"
-  FIRST_QUESTION_NAME_ITEM = ".//*[@class='list']//*[@class='dataRow even first']/th/a"
+  FIRST_QUESTION_NAME_ITEM_XPATH = ".//*[@id='bodyCell']/div[3]/div[1]/div/div[2]/table/tbody/tr[2]/th/a"
   
   
   
