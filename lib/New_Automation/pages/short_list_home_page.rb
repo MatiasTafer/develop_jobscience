a3ad5751ae78d82fad0f273fd425793de2e34679
rodrigoc@oktana.io <cmj3139cmj3139>
2016-02-16 18:45:07 -0200

    TC804 in progress

--- lib/New_Automation/pages/short_list_home_page.rb
+++ lib/New_Automation/pages/short_list_home_page.rb
@@ -2,5 +2,5 @@ class ShortListHomePage
   
   SHORT_LIST_RECORD_XPATH = ".//*[@id='bodyCell']/div[3]/div[1]/div/div[2]/table/tbody/tr[2]/th/a"
   SHORT_LIST_RECORD_XPATH_2 = ".//*[@id='bodyCell']/div[3]/div[1]/div/div[2]/table/tbody/tr[20]/th/a"
-  
+  NEW_SHORT_LIST_BUTTON_XPATH = ".//*[@class='pbButton']/input"
 end
