3fbbcba2638b25ce81d7ccaa79c20d9efc956b76
rodrigoc@oktana.io <cmj3139cmj3139>
2016-02-19 12:41:58 -0200

    TC1059 completed

---
+++ lib/New_Automation/test_requisition.rb
@@ -0,0 +1,66 @@
+require 'selenium-webdriver'
+require 'test-unit'
+require 'securerandom'
+
+
+require_relative 'test_basic.rb'
+require_relative 'common.rb'
+
+require_relative './pages/home_page.rb'
+require_relative './pages/requisitions_change_priority_page.rb'
+require_relative './pages/requisitions_change_status_page.rb'
+require_relative './pages/requisitions_close_job_page.rb'
+require_relative './pages/requisitions_detail_page.rb'
+require_relative './pages/requisitions_home_page.rb'
+require_relative './pages/requisitions_mass_transfer_page.rb'
+require_relative './pages/requisitions_new_and_edit.rb'
+
+class TestRequisition < TestBasic
+  
+#TC1059 - New Job Order 
+def test_newJobOrder
+   Common.login(Common::USER_EMAIL, Common::PASSWORD)
+   $browser.get HomePage::REQUISITION_TAB_LINK_URL
+   test = [{"displayed" => RequisitionsHomePage::REQUISITIONS_PAGE_BTN_NEW_XPATH},
+           {"click" => RequisitionsHomePage::REQUISITIONS_PAGE_BTN_NEW_XPATH},
+           {"displayed" => RequisitionsHomePage::NEW_RECORD_TYPE_DROPDOWN_XPATH},
+           {"set_text" => RequisitionsHomePage::NEW_RECORD_TYPE_DROPDOWN_XPATH, "text" => RequisitionsHomePage::RECORD_TYPE_REQUISITION_TEXT},
+           {"click" => RequisitionsHomePage::CONTINUE_BUTTON_XPATH},
+           {"displayed" => RequisitionsNewAndEdit::REQUISITIONS_NEW_JOB_TITLE_XPATH},
+           {"set_text" => RequisitionsNewAndEdit::REQUISITIONS_NEW_JOB_TITLE_XPATH, "text" => RequisitionsNewAndEdit::REQUISITION_NAME},
+           {"set_text" => RequisitionsNewAndEdit::REQUISITIONS_NEW_PRIMARY_RECRUITER_TEXT_XPATH, "text" => RequisitionsNewAndEdit::PRIMARY_RECRUITER_TEXT},
+           {"set_text" => RequisitionsNewAndEdit::REQUISITIONS_NEW_LOCATION_XPATH, "text" => RequisitionsNewAndEdit::LOCATION_TEXT},
+           {"set_text" => RequisitionsNewAndEdit::REQUISITIONS_NEW_DEPARTAMENT_XPATH, "text" => RequisitionsNewAndEdit::DEPARTMENT_TEXT},
+           {"set_text" => RequisitionsNewAndEdit::REQUISITIONS_NEW_MIN_SALARY_XPATH, "text" => RequisitionsNewAndEdit::MIN_SALARY_TEXT},
+           {"set_text" => RequisitionsNewAndEdit::REQUISITIONS_NEW_MAX_SALARY_XPATH, "text" => RequisitionsNewAndEdit::MAX_SALARY_TEXT}, 
+           {"click" => RequisitionsNewAndEdit::REQUISITIONS_NEW_BTN_SAVE_XPATH}] 
+    Common.main(test)
+    assert $wait.until {
+      $browser.find_element(:xpath, RequisitionsDetail::REQUISITIONS_DETAIL_BTN_DELETE_XPATH).displayed? 
+    }      
+end  
+
+#TC1060 - New Job Order, Validation
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
