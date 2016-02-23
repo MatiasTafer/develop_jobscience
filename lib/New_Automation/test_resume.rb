e2a0e88c8da8437955f8aab885471e692979e188
rodrigoc@oktana.io <cmj3139cmj3139>
2016-02-23 19:13:38 -0200

    Resume suite in Progress

---
+++ lib/New_Automation/test_resume.rb
@@ -0,0 +1,20 @@
+require 'selenium-webdriver'
+require 'test-unit'
+require 'securerandom'
+
+
+require_relative 'test_basic.rb'
+require_relative 'common.rb'
+
+require_relative './pages/home_page.rb'
+
+class Resume
+  
+  
+  
+  
+  
+  
+  
+  
+end
