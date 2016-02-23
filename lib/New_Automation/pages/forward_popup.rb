2cbf8a336c21ee9f8efcee94d0c07bab31e1157c
Matias t <matiast@oktana.io>
2016-02-23 11:54:06 -0300

    .

--- lib/New_Automation/pages/forward_popup.rb
+++ lib/New_Automation/pages/forward_popup.rb
@@ -2,10 +2,10 @@ class ForwardPopup
   
   # Forward pop-up
   TO_ICON_ID = "j_id0:j_id1:j_id2:frm:pbSendEmail:j_id39:j_id48:j_id52_lkwgt"
-  TO_TEXT_ID = "j_id0:j_id1:j_id2:frm:pbSendEmail:j_id39:j_id48:j_id52"
-  SUBJECT_TEXT_ID = "j_id0:j_id1:j_id2:frm:pbSendEmail:j_id39:j_id77:j_id79"
-  ATTACHMENTS_CHECKBOX_ID = "j_id0:j_id1:j_id2:frm:pbSendEmail:j_id88:j_id89:0:j_id90"
-  SEND_BUTTON_ID = "j_id0:j_id1:j_id2:frm:pbSendEmail:pbbsMainButtons:j_id35"
+  TO_TEXT_XPATH = ".//*[@id='j_id0:j_id1:j_id2:frm:pbSendEmail:j_id39:j_id48:j_id52']"
+  SUBJECT_TEXT_XPATH = ".//*[@id='j_id0:j_id1:j_id2:frm:pbSendEmail:j_id39:j_id77:j_id79']/input"
+  ATTACHMENTS_CHECKBOX_XPATH = ".//*[@id='j_id0:j_id1:j_id2:frm:pbSendEmail:j_id88:j_id89:0:j_id90']/input"
+  SEND_BUTTON_XPATH = ".//*[@value='Send']"
   SELECT_TEMPLATE_BUTTON_ID = "j_id0:j_id1:j_id2:frm:pbSendEmail:pbbsMainButtons:bottom"
   TEMPLATE_RECORD_ID = "j_id0:frm:fcf_Templates:j_id7:0:j_id10"
   
