584d80f4b79ed0089474497c45369fbfa1d9fbb7
rodrigoc@oktana.io <cmj3139cmj3139>
2016-02-26 15:43:08 -0200

    TC987 Completed

--- lib/New_Automation/test_resume.rb
+++ lib/New_Automation/test_resume.rb
@@ -275,7 +275,6 @@ def test_addResumePrivateSharingModel
   $browser.get SetupEditPage::SHARING_SETTINGS_URL
   test2 = [{"displayed" => SetupEditPage::EDIT_SHARING_SETTINGS_BUTTON_XPATH},
            {"click" => SetupEditPage::EDIT_SHARING_SETTINGS_BUTTON_XPATH},
-           #{"displayed" => SetupEditPage::CONTACT_PICKLIST_XPATH},
            {"set_text" => SetupEditPage::CONTACT_PICKLIST_XPATH, "text" => "Private"},
            {"click" => SetupEditPage::SHARING_SETTINGS_SAVE_BUTTON_XPATH}]
   Common.main(test2)
@@ -378,6 +377,47 @@ def test_contactUpdateResumeParseFields
   }   
 end
 =end
+
+#TC987 - Add resume , De-Duplication in a Private Sharing Model, Duplicate candidate not shared
+def test_addResumeNotShared
+  #Preconditions
+  Common.login(Common::USER_EMAIL, Common::PASSWORD)
+  $browser.get SetupEditPage::PARSE_SETTINGS_EDIT_URL
+  test = [{"displayed" => SetupEditPage::PARSE_SETTINGS_EDIT_BUTTON_XPATH},
+          {"click" => SetupEditPage::PARSE_SETTINGS_EDIT_BUTTON_XPATH},
+          {"set_text" => SetupEditPage::JOB_BOARD_DUPE_PREVENTION_XPATH, "text" => "Parse Fields"},
+          {"checked" => SetupEditPage::ENFORCE_SHARING_CHECKBOX_XPATH},
+          {"click" => SetupEditPage::SAVE_BUTTON_XPATH}]
+  Common.main(test)
+  $browser.get SetupEditPage::SHARING_SETTINGS_URL
+  test2 = [{"displayed" => SetupEditPage::EDIT_SHARING_SETTINGS_BUTTON_XPATH},
+           {"click" => SetupEditPage::EDIT_SHARING_SETTINGS_BUTTON_XPATH},
+           {"set_text" => SetupEditPage::CONTACT_PICKLIST_XPATH, "text" => "Private"},
+           {"click" => SetupEditPage::SHARING_SETTINGS_SAVE_BUTTON_XPATH}]
+  Common.main(test2)
+  #Steps
+  $browser.get HomePage::HOME_TAB_LINK_URL
+  test3 = [{"displayed" => HomePage::ADD_RESUMES_XPATH},
+           {"click" => HomePage::ADD_RESUMES_XPATH}]
+  Common.main(test3)
+  $wait.until{
+    windowsNumer = $browser.window_handles.size
+    windowsNumer > 1
+  }
+  newWindow= $browser.window_handles[1]
+  $browser.switch_to.window(newWindow)
+  test4 = [{"displayed" => AddResumePopUpPage::ADD_RESUME_POPUP_BTN_BROWSE_XPATH},
+           {"upload" => AddResumePopUpPage::ADD_RESUME_POPUP_BTN_BROWSE_XPATH, "file" => "/Users/admin/Desktop/document.pdf"},
+           {"click" => AddResumePopUpPage::ADD_RESUME_POPUP_BTN_ADD_RESUEM_XPATH}]
+  Common.main(test4)
+  newWindow= $browser.window_handles[0]
+  $browser.switch_to.window(newWindow)
+  assert $wait.until{
+    $browser.find_element(:xpath, ContactDetailPage::CONTACT_DETAIL_BTN_ADD_TO_LIST_XPATH).displayed?
+  }  
+end
+
+
 
 =begin
 #TC993 - Contact Update Resume Validation
