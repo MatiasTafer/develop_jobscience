53f097d2162cb40b550e7e9522a61d85680ebb04
Martin <martinds@oktana.io>
2016-03-04 16:42:00 -0300

    rollback to naomi user

--- lib/New_Automation/tests/common.rb
+++ lib/New_Automation/tests/common.rb
@@ -26,14 +26,14 @@ require './New_Automation/pages/job_board/job_board_job_detail.rb'
 
 
 class Common
-  USER_EMAIL = "oktana@joblabs.com"
+  USER_EMAIL = "naomi@joblabs.com"
   USER_EMAIL2 = "naomirecruiter@joblabs.com"
   USER_EMAIL3 = "naomimanager@joblabs.com"
   
   USER_AUTOMATION = "oktana@joblabs.com"
   PASSWORD_AUTOMATION = "Jobscience16"
   
-  PASSWORD  = "Jobscience16"
+  PASSWORD  = "muffin100"
 
   
 
