331f9c8cc739e72216b715dda395ed716e01bc68
rodrigoc@oktana.io <cmj3139cmj3139>
2016-03-07 15:50:09 -0200

    contacts_new_edit_page.rb mapping fixing

--- lib/New_Automation/pages/contacts/contacts_new_edit_page.rb
+++ lib/New_Automation/pages/contacts/contacts_new_edit_page.rb
@@ -1,13 +1,13 @@
 class ContactNewEditPage
   
   #Select Contact Record Type Section
-  CONTACT_NEW_RECORD_TYPE_NEW_XPATH = ".//*[@id='p3']"
+  CONTACT_NEW_RECORD_TYPE_NEW_XPATH = ".//*[@class='requiredInput']/select"
   CONTACT_NEW_BTN_CONTINUE_XPATH = ".//*[@title='Continue']"
   CONTACT_NEW_BTN_CANCEL_XPATH = ".//*[@title='Cancel']"
   
   #New Contact Section
-  CONTACT_NEW_LAST_NAME_INPUT_XPATH = ".//*[@id='name_lastcon2']"
-  CONTACT_NEW_ACCOUNT_NAME_INPUT_XPATH = ".//*[@id='con4']"
+  CONTACT_NEW_LAST_NAME_INPUT_XPATH = "(.//*[@class='requiredInput'])[2]/input"
+  CONTACT_NEW_ACCOUNT_NAME_INPUT_XPATH = "(.//*[@class='lookupInput'])[1]/input"
   CONTACT_NEW_FIELD_LISTS_XPATH = "(.//*[@class='detailList'])[2]"
   
   #BUTTONS
