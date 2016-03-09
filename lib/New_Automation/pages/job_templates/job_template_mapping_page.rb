1e99b397912ce10b855555c56505281d229bb414
rodrigoc@oktana.io <cmj3139cmj3139>
2016-03-09 15:08:46 -0200

    job_template_mapping_page.rb mapping fix completed

--- lib/New_Automation/pages/job_templates/job_template_mapping_page.rb
+++ lib/New_Automation/pages/job_templates/job_template_mapping_page.rb
@@ -1,6 +1,6 @@
 class JobTemplateMapping
   
-  JOB_TEMPLATE_MAPPING_NAME_XPATH = ".//*[@class='requiredInput']/input"
+  JOB_TEMPLATE_MAPPING_NAME_XPATH = "//*[text()='Name']/following-sibling::*//input"
   SAVE_BUTON_XPATH = "(.//*[@value='Save'])[1]"
   NEW_BUTTON_XPATH = ".//*[@title='New']"
   NEW_MAPPING_ITEM_BUTTON_XPATH = ".//*[@title='New Mapping Item']"
