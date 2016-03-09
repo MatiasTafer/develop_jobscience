f516024ad0a00297da8d666a36b196d21cac4da3
rodrigoc@oktana.io <cmj3139cmj3139>
2016-03-09 15:16:56 -0200

    job_template_page.rb mapping fix completed

--- lib/New_Automation/pages/job_templates/job_templates_page.rb
+++ lib/New_Automation/pages/job_templates/job_templates_page.rb
@@ -1,10 +1,10 @@
 class JobTemplates
   
-  NEW_BUTTON_XPATH = ".//*[@class='pbButton']//*[@class='btn']"
-  FIRST_JOB_TEMPLATE_IN_LIST_XPATH = "(.//*[@class='list']/tbody/tr)[2]/th"
+  NEW_BUTTON_XPATH = ".//*[@name='new']"
+  FIRST_JOB_TEMPLATE_IN_LIST_XPATH = "(//*[text()='Job Title']/../..//tr//a)[1]"
   CREATE_JOB_ORDER_BUTTON_XPATH = ".//*[@title='Create Job Order']"
   NEXT_BUTTON_XPATH = "(.//*[@value='Next'])[1]"
-  PRIMARY_RECRUITER_TEXT_FIELD_XPATH = "(.//*[@class='lookupInput'])[1]/input"
+  PRIMARY_RECRUITER_TEXT_FIELD_XPATH = "//*[text()='Primary Recruiter']/../following-sibling::*//input"
   
   ERROR_REQUIRED_FIELD_XPATH = ".//*[@class='errorMsg']"
   ERROR_REQUIRED_FIELD_TEXT = "Error: You must enter a value"
