0b02c6fd50c2d374a96684bd9f432bf8225405b5
rodrigoc@oktana.io <cmj3139cmj3139>
2016-03-09 14:43:39 -0200

    contacts_new_edit_page.rb mappong fix completed

--- lib/New_Automation/pages/contacts/contacts_new_edit_page.rb
+++ lib/New_Automation/pages/contacts/contacts_new_edit_page.rb
@@ -1,14 +1,17 @@
 class ContactNewEditPage
   
   #Select Contact Record Type Section
-  CONTACT_NEW_RECORD_TYPE_NEW_XPATH = ".//*[@class='requiredInput']/select"
+  CONTACT_NEW_RECORD_TYPE_NEW_XPATH = "//*[text()='Record Type of new record']/../following-sibling::*//select"
   CONTACT_NEW_BTN_CONTINUE_XPATH = ".//*[@title='Continue']"
   CONTACT_NEW_BTN_CANCEL_XPATH = ".//*[@title='Cancel']"
   
   #New Contact Section
-  CONTACT_NEW_LAST_NAME_INPUT_XPATH = "(.//*[@class='requiredInput'])[2]/input"
-  CONTACT_NEW_ACCOUNT_NAME_INPUT_XPATH = "(.//*[@class='lookupInput'])[1]/input"
-  CONTACT_NEW_FIELD_LISTS_XPATH = "(.//*[@class='detailList'])[2]"
+  CONTACT_NEW_LAST_NAME_INPUT_XPATH = "//*[text()='Last Name']/../..//input"
+  
+  
+  
+  CONTACT_NEW_ACCOUNT_NAME_INPUT_XPATH = "//*[text()='Account Name']/../..//input"
+  CONTACT_NEW_FIELD_LISTS_XPATH = "//*[text()='Account Name']/../../.."
   
   #BUTTONS
   CONTACT_NEW_BTN_SAVE_XPATH = ".//*[@name='save']"
