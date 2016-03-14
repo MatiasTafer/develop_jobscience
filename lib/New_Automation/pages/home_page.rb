1f28fa2e8c97c2c1f8c6560730de300d970834bc
Martin <martinds@oktana.io>
2016-03-14 11:46:39 -0300

    Project Templates

--- lib/New_Automation/pages/home_page.rb
+++ lib/New_Automation/pages/home_page.rb
@@ -2,38 +2,6 @@ class HomePage
   
   
   #TAB LINKS URL
-<<<<<<< HEAD
-  HOME_TAB_LINK_URL = ".//*[@href='/home/home.jsp']"
-  SHORT_LIST_TAB_LINK_URL = "https://na17.salesforce.com/a0r/o"
-  POWER_SEARCH_TAB_LINK_URL = "https://ts2.na17.visual.force.com/apex/JobscienceSearch?sfdc.tabName=01ro0000000WeBj"
-  REQUISITION_TAB_LINK_URL = "https://na17.salesforce.com/a0K"
-  PRESENTATION_TAB_LINK_URL = "https://na17.salesforce.com/a1v/o"
-  CONTACT_TAB_LINK_URL = "https://na17.salesforce.com/003/o"
-  AMS_SETUP_TAB_LINK_URL = "https://ts2extams.na17.visual.force.com/apex/AMSSetup?sfdc.tabName=01ro0000000hRdi"
-  BOARD_SETUP_TAB_LINK_URL = "https://na17.salesforce.com/a0G/o"
-  ADVANCED_CONTACT_SEARCH_SIDEBAR_LINK_URL = "https://ts2.na17.visual.force.com/apex/SSearch"
-  OFFERS_TAB_LINK_URL = "https://na17.salesforce.com/a0N/o"
-  APPLICATIONS_TAB_LINK_URL = "https://ts2extams.na17.visual.force.com/apex/Application_Custom_List?save_new=1&sfdc.override=1"
-  CONTACTS_TAB_LINK_URL = "https://na17.salesforce.com/003/o"
-  INTERVIEW_TAB_LINK_URL = "https://ts2extams.na17.visual.force.com/apex/Interview_Custom_List?save_new=1&sfdc.override=1"
-  PLACEMENT_TAB_LINK_URL = "https://na17.salesforce.com/a0Q/o"
-  ADVANCED_CONTACT_SEARCH_SIDEBAR_LINK_XPATH = ".//*[@class='sidebarModuleBody']//*[@class='searchFooter']/a"
-  PROJECTS_TAB_LINK_URL = "https://na17.salesforce.com/a0S/o"
-  QUESTION_SETS_LINK_URL = "https://na17.salesforce.com/a0T/o"
-  REQUISITIONS_LINK_URL = "https://na17.salesforce.com/a0K/o"
-  SOURCE_LINK_URL = "https://na17.salesforce.com/a0e/o"
-  JOB_TEMPLATES_TAB_LINK_URL = "https://na17.salesforce.com/a0B/o"
-  STANDARD_QUESTIONS_TAB_LINK = "https://na17.salesforce.com/a0f/o"
-  JOB_TEMPLATE_MAPPING_TAB_LINK = "https://na17.salesforce.com/a0J/o"
-  SUBMITTALS_TAB_LINK_XPATH = "https://ts2extams.na17.visual.force.com/apex/Submittal_Custom_List?save_new=1&sfdc.override=1"
-  CHATTER_TAB_URL = "https://na17.salesforce.com/_ui/core/chatter/ui/ChatterPage"
-  
-  LOGIN_URL = "https://login.salesforce.com/"
-
-  ALL_CONTACTS_TAB_LINK = "https://na17.salesforce.com/003?fcf=00Bo0000003aqv0"
-  
-  ACCOUNTS_TAB_LINK_URL = "https://na17.salesforce.com/001/o"
-=======
   ALL_TABS_LINK_XPATH = ".//*[@id='AllTab_Tab']"
   HOME_TAB_ID = "home_Tab"
   
@@ -68,7 +36,6 @@ class HomePage
   MENU_USER_SETUP_OPTION_XPATH = ".//*[@id='userNav-menuItems']/a[text()='Setup']"
   DEVELOP_XPATH = ".//a[text()='Develop']"
   CUSTOM_SETTINGS_XPATH = ".//*[@id='CustomSettings_font']"
->>>>>>> develop
   
   SEARCH_BAR_XPATH = ".//*[@id='phSearchInput']"
   SEARCH_BUTTON_XPATH = ".//*[@id='phSearchButton']"
