748fa23547d507a3b2a876cf0e7b24164f41f316
admin <admin@Matias-Quintana.local>
2016-03-21 09:47:35 -0300

    commit

--- lib/New_Automation.rb
+++ lib/New_Automation.rb
@@ -34,11 +34,11 @@ $wait
 
 #require './New_Automation/test_projects'
 
-require './New_Automation/tests/test_candidates_actions_on_record'
+#require './New_Automation/tests/test_candidates_actions_on_record'
 
 #require './New_Automation/tests/test_job_templates.rb'
 
-#require './New_Automation/test_short_list.rb'
+#require './New_Automation/tests/test_short_list.rb'
 
 #require './New_Automation/tests/test_standard_questions'
 
@@ -60,13 +60,13 @@ require './New_Automation/tests/test_candidates_actions_on_record'
 
 #require './New_Automation/tests/test_job_board'
 
-#require './New_Automation/tests/test_applications.rb'
+require './New_Automation/tests/test_applications.rb'
 
 #require './New_Automation/tests/test_url_builder.rb'
 
 #require './New_Automation/test_daxtra_parse.rb'
 
-require './New_Automation/tests/test_daxtra_parse.rb'
+#require './New_Automation/tests/test_daxtra_parse.rb'
 
 #require './New_Automation/test_candidates_actions_on_record'
 
